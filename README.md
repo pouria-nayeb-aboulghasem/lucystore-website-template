06d789c244b080fc340bf7628b82b45108b5cdd0
pour68 <pour68@outlook.com>
2024-05-26 00:09:13 +0330

    add mobile menu

---
+++ README.md
@@ -0,0 +1,30 @@
+# LucyStore website template
+
+Responsive cosmetics website template using HTML CSS & JavaScript.
+
+## Tools
+
+- HTML
+- CSS
+  - RemixIcon library
+- JavaScript
+  - ScrollReveal library
+
+## Features
+
+- Responsive cosmetics website template using HTML CSS And JavaScript technologies
+- Contains animations when scrolling
+- Smooth scrolling in each section
+- Contains elegant light/dark theme
+- Developed with mobile-first methodology
+- Compatible with all mobile devices with a beautiful and pleasant user interface
+
+## Screenshot
+
+## Demo
+
+[Live template](https://)
+
+## Copyright
+
+Pouria nayeb aboulghasem.
---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+const backBtns = document.querySelectorAll("#menu-back");
+const menuListCategory = document.querySelector(".menu__list:first-child");
+const menuListSubcategory = document.querySelectorAll(".menu__data");
+const closeMenu = document.querySelector("#close-menu");
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+const menu = document.querySelector("#menu");
+
+closeMenu.addEventListener("click", () => {
+    menu.classList.remove("active-menu");
+});
+
+hamburgerMenu.addEventListener("click", () => {
+    menu.classList.add("active-menu");
+});
+
+backBtns.forEach(backBtn => {
+    backBtn.addEventListener("click", () => {
+        menuListCategory.classList.remove("active");
+
+        menuListSubcategory.forEach(menuList => {
+            if (menuList.classList.contains("active-left")) {
+                menuList.classList.remove("active-left");
+            }
+        });
+    })
+});
+
+function openCity(evt, cityName) {
+    var i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("menu__data");
+
+    menuListSubcategory.forEach(menuList => {
+        if (menuList.id == cityName) {
+            menuListCategory.classList.add("active");
+            menuList.classList.add("active-left");
+        }
+    });
+
+    tabLinks = document.getElementsByClassName("menu__link--category");
+    for (i = 0; i < tabLinks.length; i++)
+        tabLinks[i].className = tabLinks[i].className.replace(" tab-active", "");
+
+    evt.currentTarget.className += " tab-active";
+}
